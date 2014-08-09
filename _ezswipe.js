cacacb9c575460ec440ff2094c6681032629ef32
Andrew Page <andrew@fuzzy-logic.co.za>
2014-08-09 07:40:55 +0200

    refactoring and cleanup

--- _ezswipe.js
+++ _ezswipe.js
@@ -1,18 +1,20 @@
 (function() {
-	function _droid_swipe(el) {
-		function dispatchEvent(name, delta) {
-			var settings = {
-				"detail" : {
+	var ezswipe_container, ezswipe_surface, i, style, _droid_swipe, _ezSwipes;
+	_droid_swipe = function(el) {
+		var dispatchEvent, _child;
+		dispatchEvent = function(name, delta) {
+			var event, settings;
+			settings = {
+				detail : {
 					delta : delta,
 					bubbles : false,
 					cancelable : true
 				}
 			};
-			var event = new CustomEvent(name, settings);
+			event = new CustomEvent(name, settings);
 			el.parentNode.dispatchEvent(event);
-			delete event;
 		};
-		var _child = el.getElementsByClassName("_ezswipe_surface")[0];
+		_child = el.getElementsByClassName("_ezswipe_surface")[0];
 		el.ezSwipe = {
 			center : {
 				x : (_child.clientWidth / 2) - (el.clientWidth / 2),
@@ -29,88 +31,98 @@
 		};
 		el.scrollLeft = el.ezSwipe.center.x;
 		el.scrollTop = el.ezSwipe.center.y;
-		el.ontouchstart = function() {
-			//NOTE: touchstart is allways fired.
-			//TODO: set initial x,y pos and calc relative values.
-		};
 		el.onscroll = function(e) {
-			//TODO: swipeCancel if edge of scroll surface reached. */
-			//TODO: Time still buggy
-			if ((el.scrollLeft != el.ezSwipe.center.x) || (el.scrollTop != el.ezSwipe.center.y)) {
+			var d, delta;
+			if ((el.scrollLeft !== el.ezSwipe.center.x) || (el.scrollTop !== el.ezSwipe.center.y)) {
 				if (!el.ezSwipe.scrolling) {
-					var delta = {
+					delta = {
 						x : 0,
 						y : 0,
 						t : new Date().getTime()
 					};
 					el.ezSwipestarttime = delta.t;
 					el.ezSwipelast = delta;
-					dispatchEvent('swipeStart', delta);
+					dispatchEvent("swipeStart", delta);
 					el.ezSwipe.scrolling = true;
 				} else {
-					var d = new Date().getTime();
-					var delta = {
-						x : (el.ezSwipe.center.x - el.scrollLeft) - el.ezSwipelast.x,
-						y : (el.ezSwipe.center.y - el.scrollTop) - el.ezSwipelast.y,
-						t : (d - el.ezSwipe.starttime) - el.ezSwipelast.t
+					d = new Date().getTime();
+					delta = {
+						x : el.ezSwipe.center.x - el.scrollLeft - el.ezSwipelast.x,
+						y : el.ezSwipe.center.y - el.scrollTop - el.ezSwipelast.y,
+						t : d - el.ezSwipe.starttime - el.ezSwipelast.t
 					};
 					el.ezSwipelast = {
 						x : el.ezSwipe.center.x - el.scrollLeft,
 						y : el.ezSwipe.center.y - el.scrollTop,
 						t : d - el.ezSwipe.starttime
 					};
-					dispatchEvent('swipeMove', delta);
+					dispatchEvent("swipeMove", delta);
 				}
 				if (el.ezSwipe.timer) {
 					clearTimeout(el.ezSwipe.timer);
 				}
 				el.ezSwipe.timer = setTimeout(function() {
+					var t;
 					el.ezSwipe.timer = null;
-					var t = parseInt(new Date().getTime());
-					var delta = {
+					t = parseInt(new Date().getTime());
+					delta = {
 						x : el.ezSwipe.center.x - el.scrollLeft,
 						y : el.ezSwipe.center.y - el.scrollTop,
 						t : t - el.ezSwipe.starttime
 					};
 					el.ezSwipelast = delta;
-					dispatchEvent('swipeEnd', delta);
+					dispatchEvent("swipeEnd", delta);
 					if (Math.abs(delta.y) > Math.abs(delta.x)) {
-						delta.y < 0 ? dispatchEvent('swipeUp', delta) : dispatchEvent('swipeDown', delta);
+						if (delta.y < 0) {
+							dispatchEvent("swipeUp", delta);
+						} else {
+							dispatchEvent("swipeDown", delta);
+						}
 					} else {
-						delta.x < 0 ? dispatchEvent('swipeLeft', delta) : dispatchEvent('swipeRight', delta);
+						if (delta.x < 0) {
+							dispatchEvent("swipeLeft", delta);
+						} else {
+							dispatchEvent("swipeRight", delta);
+						}
 					}
 					el.ezSwipe.scrolling = false;
 					_droid_swipe(el);
 				}, 100);
 			}
 		};
-	}
-
-	var _ezSwipes = document.getElementsByClassName("_ezswipe");
-	for (var i = 0; i < _ezSwipes.length; i++) {
-		var ezswipe_container = document.createElement("div");
+	};
+	_ezSwipes = document.getElementsByClassName("_ezswipe");
+	i = 0;
+	while (i < _ezSwipes.length) {
+		ezswipe_container = document.createElement("div");
 		ezswipe_container.classList.add("_ezswipe_container");
-		_ezSwipes[i].style.overflow = 'hidden';
-		with (ezswipe_container.style) {
-			webkitUserSelect = 'none';
-			webkitOverflowScrolling = 'touch';
-			overflow = 'auto';
-			opacity = 0;
-			top = 0;
-			left = 0;
-			position = 'absolute';
-			_ezSwipes[i].offsetWidth > document.width ? width = document.width + 'px' : width = _ezSwipes[i].offsetWidth + 'px';
-			_ezSwipes[i].offsetHeight > document.height ? height = document.height + 'px' : height = _ezSwipes[i].offsetHeight + 'px';
-		};
-		var ezswipe_surface = document.createElement("div");
+		_ezSwipes[i].style.overflow = "hidden";
+		style = ezswipe_container.style;
+			style.webkitUserSelect = "none";
+			style.webkitOverflowScrolling = "touch";
+			style.overflow = "auto";
+			style.opacity = 0;
+			style.top = 0;
+			style.left = 0;
+			style.position = "absolute";
+		if (_ezSwipes[i].offsetWidth > document.width) {
+			style.width = document.width + "px";
+		} else {
+			style.width = _ezSwipes[i].offsetWidth + "px";
+		}
+		if (_ezSwipes[i].offsetHeight > document.height) {
+			style.height = document.height + "px";
+		} else {
+			style.height = _ezSwipes[i].offsetHeight + "px";
+		}
+		ezswipe_surface = document.createElement("div");
 		ezswipe_surface.classList.add("_ezswipe_surface");
-		with (ezswipe_surface.style) {
-			position = 'relative';
-			width = '300%';
-			height = '300%';
-		};
+		style = ezswipe_surface.style;
+			style.position = "relative";
+			style.width = "300%";
+			style.height = "300%";
 		ezswipe_container.appendChild(ezswipe_surface);
 		_droid_swipe(_ezSwipes[i].insertBefore(ezswipe_container, _ezSwipes[i].firstElementChild));
+		i++;
 	}
 })();
-
